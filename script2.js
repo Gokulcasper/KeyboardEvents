b36adc1291c6fa61f770543ce82ab4122190c7e1
Gokulcasper <ronitcasper215@gmail.com>
2022-07-01 14:37:40 +0530

    first commit

---
+++ script2.js
@@ -0,0 +1,37 @@
+// Keyboard Events :
+
+// // Keypress :
+// document.addEventListener("keypress", (event) => {
+//   console.log("Keypress", event);
+// });
+
+// // Keydown :
+// document.addEventListener("keydown", (event) => {
+//   console.log("Keydown", event);
+// });
+
+// Keyup :
+// document.addEventListener("keyup", (event) => {
+//   console.log(`keyup : key : ${event.key},code : ${event.code}`);
+//   console.log(event);
+// });
+
+// document.addEventListener("keyup", (event) => {
+//   switch (event.key) {
+//     case "ArrowUp":
+//       alert("You Enter Up Arrow Key");
+//       break;
+//     case "ArrowDown":
+//       alert("You Enter Down Arrow Key");
+//       break;
+//     case "ArrowRight":
+//       alert("You Enter Right Arrow Key");
+//       break;
+//     case "ArrowLeft":
+//       alert("You Enter Left Arrow Key");
+//       break;
+//     default:
+//       alert("Unknown Key");
+//       break;
+//   }
+// });
